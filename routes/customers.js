030ea8078a8d49822f0efa5bd9fb6977a5da1cc5
snorriarinbjarnar@gmail.com <snorriarinbjarnar@gmail.com>
2019-04-15 16:13:18 +0000

    get customer by id

--- routes/customers.js
+++ routes/customers.js
@@ -33,4 +33,28 @@ const Customer = require('../schemas/Customer');
   
   });
 
+  router.get('/:_id', (req, res, next) => {
+    const id = req.params._id;
+    Customer
+        //.find({'_id' : id})
+        .findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            if(doc){
+                res.status(200).json(doc);
+            }
+            else {
+                res.status(404).json({message: 'No valid entry found'});
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });  //If i dont pass anything it will find all elements
+  
+  });
+
   module.exports = router;
