25143621b3ef73e60c7e4bdab8858acbebeecc0c
Clover <smarig16@ru.is>
2019-04-20 20:03:38 +0000

    Backing up before pull

--- mongo.js
+++ mongo.js
@@ -3,8 +3,12 @@ const express = require('express');
 
 require('dotenv').config();
 
-const connection = mongoose.connect(process.env.DB_CONN || APPSETTING_DB_CONN, {
-   
+// const connection = mongoose.connect(process.env.DB_CONN || APPSETTING_DB_CONN, {
+
+//   useNewUrlParser: true
+// });
+
+const connection = mongoose.connect(process.env.DB_CONN, {
   useNewUrlParser: true
 });
 
